3e51d17ddf34828cceebd86479d42e31632e8493
sarora420 <86275696+sarora420@users.noreply.github.com>
2021-06-25 01:28:38 -0400

    Add files via upload

---
+++ FinalProject/src/FirstPackage/Project_class.java
@@ -0,0 +1,26 @@
+package FirstPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class Project_class
+	{
+
+    @Test
+    public void testlogo(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\localadmin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        String actualTitle = "Google";
+        Assert.assertEquals(driver.getTitle(), actualTitle );
+       WebElement logo = driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/img")  );
+      Assert.assertEquals(true, logo.isDisplayed());
+        System.out.println("pass");
+        driver.close();
+}
+}
+
+
+
